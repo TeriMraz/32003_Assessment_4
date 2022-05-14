aa58fb1d6c18dc858ff1b209282bb7304a3bc064
Jiawei Wang <1547617065@qq.com>
2022-05-14 15:08:53 +1000

    setup

---
+++ Assets/Scenes/Scripts/Level4Script/Monster400.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster400 : MonoBehaviour
+{
+    private Transform[] positions;
+
+    public int index = 0;
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (positions == null)
+        {
+            positions = L4WayPoint.L4wayPoint;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        if(positions != null)
+        {
+            transform.Translate((positions[index].position - transform.position).normalized * speed * Time.deltaTime);
+            if (Vector3.Distance(positions[index].position, transform.position) < 0.5f)
+            {
+                index++;
+
+                if (index == 4)
+                {
+                    index = 0;
+                    index++;
+                }
+            }
+        }
+        
+    }
+}
